f5967c36c2aff3b9bc7cd3a79466835ee2ee3bde
COUCOU128 <95702644+COUCOU128@users.noreply.github.com>
2021-12-10 13:14:17 +0100

    Add files via upload

---
+++ _next/static/RfBhzoNdbzEsAsSr_K5-4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/329-bb31ea77c863c62c4f42.js","static/chunks/pages/index-3200e159357b188499d0.js"],"/_error":["static/chunks/pages/_error-6dc8f004f8d8272c84b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
